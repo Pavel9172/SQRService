6cae774cdfff47df0c1ced0bab06cd9ee2769b69
Pavel9172 <pavlo_k@mail.ru>
2022-04-14 20:29:32 +0500

    int J

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,9 +3,10 @@ package ru.netology.sqr;
 public class SQRService {
     public static void main(String[] args) {
 
+        int j = 0;
         for (int i = 10; i <= 99; i++){
             if (i * i >= 100 && i * i <= 500){
-                int j = 0;
+
 
 
                 System.out.println( "значения" +i);
