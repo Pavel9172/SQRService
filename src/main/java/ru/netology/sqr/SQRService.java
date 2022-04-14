a7397776b7ec3723745ad702231526ef5c42e695
Pavel9172 <pavlo_k@mail.ru>
2022-04-14 22:08:55 +0500

    changing the code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,12 +4,15 @@ public class SQRService {
     public static void main(String[] args) {
 
         int j = 0;
+
         for (int i = 10; i <= 99; i++){
             if (i * i >= 100 && i * i <= 500){
+                  j++;
 
 
 
-                System.out.println( "значения" +i);
+
+                System.out.println( "значения" +j);
 
 
 
