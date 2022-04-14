dd57e9406bbb058d0739a69192ee9d240b8166fb
Pavel9172 <pavlo_k@mail.ru>
2022-04-14 20:12:01 +0500

    added gitignore

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,21 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public static void main(String[] args) {
+
+        for (int i = 10; i <= 99; i++){
+            if (i * i >= 100 && i * i <= 500){
+                int j = 0;
+
+
+                System.out.println( "значения" +i);
+
+
+
+
+            }
+        }
+        System.out.println("fdfdf"  );
+    }
+
+}
