26bd52798783b58ad20f6958e6e424a3746038c4
Pavel9172 <pavlo_k@mail.ru>
2022-04-21 10:44:06 +0500

    adding tests

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,11 +8,47 @@ class SQRServiceTest {
     SQRService service = new SQRService();
 
     @Test
-    void calculte() {
+    void calculteActual() {
 
         int min = 200;
         int max = 300;
         int expected = 3;
+        int actual = service.calculateAmountSquare(min, max);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void calculteMaximumNumber() {
+
+        int min = 2;
+        int max = 9000000;
+        int expected = 90;
+        int actual = service.calculateAmountSquare(min, max);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void calculteMinimumNumber() {
+
+        int min = 10;
+        int max = 100;
+        int expected = 1;
+        int actual = service.calculateAmountSquare(min, max);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void calculteNumberNotParameters() {
+
+        int min = 1;
+        int max = 50;
+        int expected = 0;
         int actual = service.calculateAmountSquare(min, max);
         assertEquals(expected, actual);
 
